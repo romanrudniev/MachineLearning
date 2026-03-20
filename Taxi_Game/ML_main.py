192de7d118129129eafbd01a1c6a551d09ac81ab
romanrudniev <romanrudniev007@gmail.com>
2026-03-20 12:40:40 +0200

    add passenger

--- Taxi_Game/ML_main.py
+++ Taxi_Game/ML_main.py
@@ -5,16 +5,11 @@ import numpy as np
 
 width, height = 700, 450
 FPS = 60
-BLACK = (0, 0, 0)
 
-x_direction = 0
-y_direction = 0
-
-player_speed = 2
 pg.init()
-
 screen = pg.display.set_mode([width, height])
 timer = pg.time.Clock()
+
 images_dict = {
     'bg': pg.image.load('img/Background.png'),
     'player': {
@@ -23,38 +18,24 @@ images_dict = {
         'front': pg.image.load('img/cab_front.png'),
         'right': pg.image.load('img/cab_right.png'),
     },
-    'hole': pg.image.load('img/hole.png'),
     'hotel': pg.transform.scale(pg.image.load('img/hotel.png'), (40, 40)),
     'passenger': pg.image.load('img/passenger.png'),
     'parking': pg.transform.scale(pg.image.load('img/parking.png'), (80, 45)),
 }
 
 def is_crash():
-    for x in range(player_rect.x, player_rect.topright[0], 1):
-        for y in range(player_rect.y, player_rect.bottomleft[1], 1):
-            try:
-                if screen.get_at((x, y)) == (220, 215, 177) or screen.get_at((x, y)) == (155, 144, 122, 255) or screen.get_at((x, y)) == (216, 211, 175, 255) or screen.get_at((x, y)) == (166, 164, 139, 255) or screen.get_at((x, y)) == (149, 147, 135, 255) or screen.get_at((x, y)) == (65, 113, 26, 255) or screen.get_at((x, y)) == (104, 132, 69, 255):
-                    return True
-            except IndexError:
-                print("Назад в діапазон!!")
-                return True
-
-    if hotel_rect.colliderect(player_rect):
-        return True
-
-    return False
+    return hotel_rect.colliderect(player_rect)
 
 def draw_message(text, color):
     font = pg.font.SysFont(None, 36)
     message = font.render(text, True, color)
-    screen.blit(message, (350, 150))
+    screen.blit(message, (250, 150))
     pg.display.flip()
     pg.time.delay(1500)
 
 player_view = 'rear'
 player_rect = images_dict['player'][player_view].get_rect()
-player_rect.x = 300
-player_rect.y = 300
+player_rect.x, player_rect.y = 300, 300
 
 hotel_img = images_dict['hotel']
 hotel_rect = hotel_img.get_rect()
@@ -66,145 +47,166 @@ hotel_positions = [
     (445, 300)
 ]
 
+# готель
 hotel_rect.x, hotel_rect.y = random.choice(hotel_positions)
 
+# парковка
 parking_img = images_dict['parking']
 parking_rect = parking_img.get_rect()
-parking_rect.x, parking_rect.y = hotel_rect.x, hotel_rect.y + hotel_rect.height
+parking_rect.x = hotel_rect.x
+parking_rect.y = hotel_rect.y + hotel_rect.height
 
+# пасажир (НЕ в готелі)
 passenger_img = images_dict['passenger']
 passenger_rect = passenger_img.get_rect()
-(passenger_rect.x, passenger_rect.y) = random.choice(hotel_positions)
+
+available_positions = [pos for pos in hotel_positions if pos != (hotel_rect.x, hotel_rect.y)]
+passenger_rect.x, passenger_rect.y = random.choice(available_positions)
 passenger_rect.y += hotel_rect.height
+
+has_passenger = False
 
 def apply_action(action):
     global player_view
-    x_direction = 0
-    y_direction = 0
-    if action is None:
-        return
-    if action == 0:
-        x_direction = 1
-        player_view = 'right'
-    elif action == 1:
-        x_direction = -1
-        player_view = 'left'
-    elif action == 2:
-        y_direction = -1
-        player_view = 'rear'
-    elif action == 3:
-        y_direction = 1
-        player_view = 'front'
+    x_dir, y_dir = 0, 0
 
-    new_x = player_rect.x + player_rect.width * x_direction
-    new_y = player_rect.y + player_rect.height * y_direction
+    if action == 0:
+        x_dir, player_view = 1, 'right'
+    elif action == 1:
+        x_dir, player_view = -1, 'left'
+    elif action == 2:
+        y_dir, player_view = -1, 'rear'
+    elif action == 3:
+        y_dir, player_view = 1, 'front'
+
+    new_x = player_rect.x + player_rect.width * x_dir
+    new_y = player_rect.y + player_rect.height * y_dir
 
     if 0 < new_x < width - player_rect.width:
         player_rect.x = new_x
     if 0 < new_y < height - player_rect.height:
         player_rect.y = new_y
 
-actions = [0, 1, 2, 3] # 0-right 1-left 2-up 3-bottom
+def draw_elements():
+    screen.blit(images_dict['bg'], (0, 0))
+    screen.blit(hotel_img, hotel_rect)
+    screen.blit(parking_img, parking_rect)
+
+    if not has_passenger:
+        screen.blit(passenger_img, passenger_rect)
+
+    screen.blit(images_dict['player'][player_view], player_rect)
+    pg.display.flip()
+
+actions = [0, 1, 2, 3]
 Q_table = defaultdict(lambda: [0, 0, 0, 0])
 
 learning_rate = 0.9
 discount_factor = 0.9
-# epsilon = -1
-epsilon = 0.1
+epsilon = 0.3
 
 def choose_action(state):
     if random.random() < epsilon:
         return random.choice(actions)
-    else:
-        return np.argmax(Q_table[state])
+    return np.argmax(Q_table[state])
 
 def update_q(state, action, reward, next_state):
     best_next = max(Q_table[next_state])
-    Q_table[state][action] += learning_rate * (reward + discount_factor * best_next - Q_table[state][action])
+    Q_table[state][action] += learning_rate * (
+        reward + discount_factor * best_next - Q_table[state][action]
+    )
+
+def get_distance(target):
+    dx = player_rect.centerx - target.centerx
+    dy = player_rect.centery - target.centery
+    return (dx**2 + dy**2) ** 0.5
 
 def make_step():
-    current_state = (player_rect.x, player_rect.y)
+    global has_passenger
+
+    current_state = (player_rect.x, player_rect.y, has_passenger)
     action = choose_action(current_state)
 
-    reward = -1
+    target = passenger_rect if not has_passenger else parking_rect
+    old_distance = get_distance(target)
+
+    reward = -0.1
     episode_end = False
     success = False
+
     apply_action(action)
+    draw_elements()
+
     if is_crash():
-        reward = -200
+        reward = -50
         episode_end = True
 
-    if parking_rect.contains(player_rect):
-        reward = 200
+    elif not has_passenger and player_rect.colliderect(passenger_rect):
+        has_passenger = True
+        reward = 20
+
+    elif has_passenger and parking_rect.contains(player_rect):
+        reward = 50
         episode_end = True
         success = True
 
-    next_state = (player_rect.x, player_rect.y)
+    else:
+        target = passenger_rect if not has_passenger else parking_rect
+        new_distance = get_distance(target)
+
+        if new_distance < old_distance:
+            reward += 1
+        else:
+            reward -= 0.5
+
+    next_state = (player_rect.x, player_rect.y, has_passenger)
     update_q(current_state, action, reward, next_state)
 
-    return(episode_end, success)
+    return episode_end, success
 
-num_episodes = 300
-max_step = 50
-for episode in range(num_episodes):
-    player_rect.x, player_rect.y = 300, 300
-    for step in range(max_step):
-        (episode_end, success) = make_step()
-        if episode_end:
-            print(success)
+# НАВЧАННЯ
+for episode in range(300):
+    player_rect.x, player_rect.y = 280, 300
+    has_passenger = False
+
+    for _ in range(50):
+        end, success = make_step()
+        if end:
             break
 
-print(Q_table)
 draw_message("Навчання завершено", pg.Color('blue'))
 
+# ТЕСТ
+player_rect.x, player_rect.y = 280, 300
+player_view = 'rear'
+has_passenger = False
+epsilon = -1
+
+FPS = 20
 run = True
+
 while run:
     timer.tick(FPS)
+
+    state = (player_rect.x, player_rect.y, has_passenger)
+    action = choose_action(state)
+    apply_action(action)
 
     for event in pg.event.get():
         if event.type == pg.QUIT:
             run = False
 
-    player_rect.x += player_speed * x_direction
-    player_rect.y += player_speed * y_direction
-
-    x_direction = 0
-    y_direction = 0
-
     if is_crash():
-        print("IS CRASH")
-        player_view = 'rear'
-        player_rect.x = 300
-        player_rect.y = 300
-        (passenger_rect.x, passenger_rect.y) = random.choice(hotel_positions)
-        passenger_rect.y += hotel_rect.height
-        continue
+        draw_message("IS CRASH", pg.Color('red'))
+        break
 
-    if parking_rect.contains(player_rect):
-        draw_message("Перемога!!!", pg.Color('green'))
+    if not has_passenger and player_rect.colliderect(passenger_rect):
+        has_passenger = True
 
-        player_view = 'rear'
-        player_rect.x = 300
-        player_rect.y = 300
+    if has_passenger and parking_rect.contains(player_rect):
+        draw_message("Перемога!", pg.Color('green'))
+        break
 
-        hotel_rect.x, hotel_rect.y = random.choice(hotel_positions)
-        parking_rect.x, parking_rect.y = hotel_rect.x, hotel_rect.y + hotel_rect.height
-
-        (passenger_rect.x, passenger_rect.y) = random.choice(hotel_positions)
-        passenger_rect.y += hotel_rect.height
-        continue
-
-    if player_rect.colliderect(passenger_rect):
-        passenger_rect.x, passenger_rect.y = player_rect.x, player_rect.y
-
-    screen.fill(BLACK)
-    screen.blit(images_dict['bg'], (0, 0))
-
-    screen.blit(hotel_img, hotel_rect)
-    screen.blit(parking_img, parking_rect)
-    screen.blit(passenger_img, passenger_rect)
-    screen.blit(images_dict['player'][player_view], player_rect)
-
-    pg.display.flip()
+    draw_elements()
 
 pg.quit()
